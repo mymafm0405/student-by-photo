f9de33f1129825d81d13a4c017272794245181c1
Mahmoud <mymafm0405@gmail.com>
2022-01-31 09:21:50 +0300

    sound changed

--- src/app/components/student/student.component.ts
+++ src/app/components/student/student.component.ts
@@ -3,20 +3,18 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-student',
   templateUrl: './student.component.html',
-  styleUrls: ['./student.component.css']
+  styleUrls: ['./student.component.css'],
 })
 export class StudentComponent implements OnInit {
   @Input() studentIndex: number;
   showImage = false;
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onStudentClick() {
     this.showImage = true;
-    const audio = new Audio('../assets/click.mp3');
+    const audio = new Audio('../assets/click2.mp3');
     audio.play();
   }
-
 }
